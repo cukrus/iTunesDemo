4ca61a7ac8c412b6029ef95a2b4d8cf167371101
Cukrus-PC <cukruss@gmail.com>
2021-10-10 16:56:05 +0300

    adding initial project files from spring initializer

---
+++ src/main/java/net/cukrus/itunesdemo/TopArtistAlbumsApplication.java
@@ -0,0 +1,13 @@
+package net.cukrus.itunesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopArtistAlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopArtistAlbumsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cukrus/itunesdemo/TopArtistAlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cukrus.itunesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopArtistAlbumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
